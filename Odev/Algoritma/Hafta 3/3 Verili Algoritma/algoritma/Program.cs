7f3eb60050d89954e15aee126fe5de6faaadd97b
alibiner <alibiner.github@gmail.com>
2022-12-10 20:30:50 +0300

    3.Hafta Ödevi

---
+++ Odev/Algoritma/Hafta 3/3 Verili Algoritma/algoritma/Program.cs
@@ -0,0 +1,70 @@
+﻿
+Console.WriteLine("1.Sayıyı Giriniz:");
+int s1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2.Sayıyı Giriniz:");
+int s2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("3.Sayıyı Giriniz:");
+int s3 = Convert.ToInt32(Console.ReadLine());
+
+
+int k = 0;
+int orta = 0;
+int enb = 0;
+
+//En Büyük Sayı Bulma
+if (s1>=s2 && s1>=s3)
+{
+    enb = s1;
+}
+else if (s2>=s1 && s2>=s3)
+{
+    enb = s2;
+}
+else if (s3 >= s2 && s3 >= s1)
+{
+    enb=s3;
+}
+
+
+//En Büyüğün s1 Olma İhtimaline Göre  Orta ve Küçüğü Bulma
+if (enb==s1 && s2>=s3)
+{
+    orta = s2;
+    k = s3;
+}
+else if (enb == s1 && s3 >=s2)
+{
+    orta = s3;
+    k = s2;
+}
+
+
+//En Büyüğün s2 Olma İhtimaline Göre Orta ve Küçüğü Bulma
+if (enb == s2 && s1 >= s3)
+{
+    orta = s1;
+    k = s3;
+}
+else if(enb == s2 && s3 >= s1)
+{
+    orta = s3;
+    k = s1;
+}
+
+
+//En Büyüğün s3 Olma İhtimaline Göre Orta ve Küçüğü Bulma
+if (enb == s3 && s2 >= s1)
+{
+    orta = s2;
+    k = s1;
+}
+else if (enb == s3 && s1 >= s2)
+{
+    orta = s1;
+    k = s2;
+}
+
+Console.WriteLine("\n {0} \n {1} \n {2} ",enb,orta,k);
+Console.ReadLine();
---
+++ Odev/Algoritma/Hafta 3/Sıralama/Sıralama/Siralama/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+
+
+
+
+Console.WriteLine("Girilecek Sayı Adedini Yazınız:");
+int numberPiece = Convert.ToInt32(Console.ReadLine());
+int[] dataList = new int[numberPiece];
+
+for (int i = 1; i <= numberPiece; i++)
+{
+    Console.WriteLine("{0}. Sayıyı Giriniz:",i);
+    int temporaryData = Convert.ToInt32(Console.ReadLine());
+    dataList[i-1] = temporaryData;
+
+
+}
+
+
+int temp;
+for (int i = 0; i < numberPiece-1; i++)
+{
+    for (int j = 0; j < numberPiece-1; j++)
+    {
+        if (dataList[j] > dataList[j+1])
+        {
+            temp = dataList[j + 1];
+            dataList[j + 1] = dataList[j];
+            dataList[j] = temp;
+        }
+    }
+}
+
+
+
+
+
+for (int i = 0; i < numberPiece; i++)
+{
+    Console.WriteLine("{0}", dataList[i]);
+}
+
+
+
+
+
+
+
